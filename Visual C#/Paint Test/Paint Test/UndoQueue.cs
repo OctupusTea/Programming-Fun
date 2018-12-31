c59fb1b7e29fbf4e992375deffe00759f779290b
OctupusTea <eland.tea@gmail.com>
2018-12-31 23:56:10 +0800

    Paint Test

---
+++ Visual C#/Paint Test/Paint Test/UndoQueue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace Paint_Test
+{
+	public class UndoList
+	{
+		private Stack<List<Line>> undoStack;
+		private Stack<List<Line>> redoStack;
+
+		public const int DEFAULT_CAPACITY = 15;
+		UndoList( int capacity = DEFAULT_CAPACITY )
+		{
+			undoStack = new Stack<List<Line>>( capacity );
+			redoStack = new Stack<List<Line>>( capacity );
+		}
+
+		void Insert( List<Line> lineList )
+		{
+			
+		}
+	}
+}
+
